619a3f4a56562a3a76001d4b2c2e094369057eb5
Ben <Benjamink1409@gmail.com>
2020-11-26 17:18:30 -0500

    refactored for task1

--- Task1/scripts/craft_adversarial_examples.py
+++ Task1/scripts/craft_adversarial_examples.py
@@ -79,16 +79,16 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="")
 
     parser.add_argument('-m', '--model-configs', required=False,
-                        default='../configs/experiment/model-mnist.json',
+                        default='../models/model-mnist.json',
                         help='Folder where models stored in.')
     parser.add_argument('-d', '--data-configs', required=False,
-                        default='../configs/experiment/data-mnist.json',
+                        default='../configs/data-mnist.json',
                         help='Folder where test data stored in.')
     parser.add_argument('-a', '--attack-configs', required=False,
-                        default='../configs/experiment/attack-zk-mnist.json',
+                        default='../configs/attack-zk-mnist.json',
                         help='Folder where test data stored in.')
     parser.add_argument('-o', '--output-root', required=False,
-                        default='../configs/experiment/results',
+                        default='../data',
                         help='Folder for outputs.')
     parser.add_argument('--debug', required=False, default=True)
 
